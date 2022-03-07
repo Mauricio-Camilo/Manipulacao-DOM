2bcfbdffe7a60ab1b2567c883c5a1fe5e6e25bed
Mauricio-Camilo <mauricio.ecamilo@gmail.com>
2022-03-07 16:24:21 -0300

    demonstração de conceitos da apresentação

---
+++ scripts.js
@@ -0,0 +1,99 @@
+// window.alert("ola mundo");
+// window.prompt("digite seu nome");
+// const nome = "Mauricio";
+// window.console.log(nome);
+
+// const listaPratos2 = document.querySelector("ul");
+// // const elementos = listaPratos.childElementCount;
+// // console.log (elementos);
+// const nodes = listaPratos2.childNodes;
+// console.log(nodes);
+
+
+// const listaPratos = document.querySelectorAll("li");
+// console.log(listaPratos[0].innerHTML);
+
+// const listaManual = ["Pizza","Macarronada","Feijoada","Churrasco"];
+// console.log(listaManual[0]);
+
+// const listaPratos = document.querySelectorAll("li");
+// let array1 = [...listaPratos];
+// console.log(array1);
+// let array2 = Array.from(listaPratos);
+// console.log(array2);
+
+// const listaPratos = document.querySelector("#pratos li");
+// console.log(listaPratos);
+
+// const listaComidas = document.getElementById("pratos");
+// console.log(listaComidas);
+
+// const listaPratos = document.querySelectorAll("li")
+// console.log(listaPratos);
+
+// const listaComidas = document.getElementsByClassName("prato");
+// console.log(listaComidas);
+
+// const pratos = document.getElementsByClassName("prato");
+// console.log(pratos);
+// pratos.forEach (prato => {
+//     console.log(prato);
+// })
+
+// const lista = document.querySelector("ul");
+// const lista1 = lista.innerText;
+// console.log(lista1);
+// const lista2 = lista.innerHTML;
+// console.log(lista2);
+// const lista3 = lista.textContent;
+// console.log(lista3);
+
+// const novaDiv = document.createElement("div");
+// novaDiv.classList.add("nova-classe");
+// const novoParagrafo = document.createElement("p");
+// novoParagrafo.innerHTML += "Criar elementos aqui";
+// console.log(novoParagrafo);
+// novaDiv.appendChild(novoParagrafo);
+// console.log (novaDiv);
+// const body = document.querySelector("body");
+// body.appendChild(novaDiv);
+
+// let listaPratos = document.getElementById("pratos");
+// const listaItens = document.getElementsByTagName("li");
+// const botao = document.querySelector("button");
+// botao.addEventListener('click',() => {
+//     const li = document.createElement("li");
+//     li.textContent += "Pastel";
+//     listaPratos.appendChild(li);
+//     console.log(listaItens.length);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
